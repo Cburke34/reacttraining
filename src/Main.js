cf1f51ddf3a0fe998fa0583c7e1c289996b16db0
Chris Burke <burkech@umail.iu.edu>
2017-07-09 03:19:07 -0500

    Removed some placeholders and began creating main component

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Main.css'
+
+const Main = () => {
+    return (
+    <div ClassName="Main">
+    <Title />
+    <Body />
+    <Bottom />
+    <div/>
+    )
+}
+
+export default Main
